d7440e9a6ea437621156b6907cf1055767b51e15
VikaJobs <56188359+VikaJobs@users.noreply.github.com>
2023-03-17 09:13:51 +0000

    Deploying to gh-pages from @ VikaJobs/goit-js-hw-09@da4db45470759931cc1de4810b8516f82f57bad0 🚀

---
+++ 01-color-switcher.20bb215f.js
@@ -0,0 +1,2 @@
+const t={body:document.body,btnStart:document.querySelector('button[data-start = "start"]'),btnStop:document.querySelector('button[data-stop = "stop"]')};let n=null;t.btnStart.addEventListener("click",(function(){n=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.btnStart.disabled=!0})),t.btnStop.addEventListener("click",(function(){clearInterval(n),t.btnStart.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.20bb215f.js.map
