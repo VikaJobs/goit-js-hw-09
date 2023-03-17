da4db45470759931cc1de4810b8516f82f57bad0
VikaJobs <rabotilova@gmail.com>
2023-03-17 11:13:21 +0200

    init

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const refs = {
+  body: document.body,
+  btnStart: document.querySelector('button[data-start = "start"]'),
+  btnStop: document.querySelector('button[data-stop = "stop"]'),
+};
+
+const INTERVAL_DELAY = 1000;
+let intervalId = null;
+
+refs.btnStart.addEventListener('click', changeColor);
+refs.btnStop.addEventListener('click', onBtnStop);
+
+function changeColor() {
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, INTERVAL_DELAY);
+  refs.btnStart.disabled = true;
+}
+
+function onBtnStop() {
+  clearInterval(intervalId);
+  refs.btnStart.disabled = false;
+}
