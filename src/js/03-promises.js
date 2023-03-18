8d5e997942dbfbc4d9468ec4420aa3870a33a3b5
VikaJobs <rabotilova@gmail.com>
2023-03-18 16:25:07 +0200

    init

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+};
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+function onError({ position, delay }) {
+  Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+}
+
+function onSuccess({ position, delay }) {
+  Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function onCreatePromises(e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const amount = Number(formData.get('amount'));
+  const delay = Number(formData.get('delay'));
+  const step = Number(formData.get('step'));
+
+  for (let i = 1; i <= amount; i += 1) {
+    const promiseDelay = delay + (i - 1) * step;
+    createPromise(i, promiseDelay).then(onSuccess).catch(onError);
   }
 }
+
+refs.form.addEventListener('submit', onCreatePromises);
